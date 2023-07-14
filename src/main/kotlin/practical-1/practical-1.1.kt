8a98279e4e539448d49d61343f905c869e9166ae
student <student@gnu.ac.in>
2023-07-14 08:29:20 +0530

    Created practical-1.1.kt

---
+++ src/main/kotlin/practical-1/practical-1.1.kt
@@ -0,0 +1,36 @@
+package `practical-1`
+
+import com.sun.jdi.ShortType
+
+fun main ()
+{
+    println("Hello World!")
+    var a: Int = 22
+    var b : Float = 1.5f
+    var c : Char = 'T'
+    var d : String = "HMS"
+    var e : Boolean = false
+    var f : Double = 96.36
+    var g : Long = 338927847
+    var h : Short = -2
+    var i : Byte = 127
+
+    println("Interger Value: $a")
+    println("Float Value: $b")
+    println("Character Value: $c")
+    println("String Value: $d")
+    println("Boolean Value: $e")
+    println("Double Value: $f")
+    println("Long Value: $g")
+    println("Short Value: $h")
+    println("Byte Value: $i")
+
+
+
+
+
+
+
+
+
+}
