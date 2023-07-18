7ebd49e6d2e81feddd72de9d0cc65a719754fc03
student <student@gnu.ac.in>
2023-07-18 08:50:26 +0530

    Completed practical-1.3.kt

---
+++ src/main/kotlin/practical-1/practical-1.3.kt
@@ -0,0 +1,47 @@
+package `practical-1`
+
+fun main (){
+
+
+
+    print("Enter Student Enrollment no : ")
+    var enrollment = readLine()
+
+    print("Enter Student's Name : ")
+    var studentName = readLine()
+
+    print("Enter Student's Branch :")
+    var branch = readLine()
+
+    print("Enter Student's Class :")
+    var studentClass = readLine()
+
+    print("Enter Student's batch : ")
+    var batch = readLine()
+
+    print("Enter Student's College Name :")
+    var collegeName = readLine()
+
+    print("Enter Student's University Name :")
+    var universityName = readLine()
+
+    print("Enter Student's Age :")
+    var age = readLine()
+
+    println("****************   Student Information    ****************")
+    println("Enrollment No : $enrollment")
+    println("Name : $studentName")
+    println("Branch: $branch")
+    println("Batch: $batch")
+    println("College: $collegeName")
+    println("University: $universityName")
+    println("Age: $age")
+
+
+
+
+
+
+
+
+}
