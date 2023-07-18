60f140f4b239acc67c01db845659fc021a40ff81
student <student@gnu.ac.in>
2023-07-18 08:22:05 +0530

    Completed practical-1.2.kt

---
+++ src/main/kotlin/practical-1.2.kt
@@ -0,0 +1,22 @@
+fun main (){
+    var integerValue = 10
+    println("Integer value : $integerValue")
+
+    var integerToDouble = integerValue.toDouble()
+    println("Double valur (From Integer) : $integerToDouble")
+
+    var stringValue = "10"
+    println("String Value : $stringValue")
+
+    var stringToInteger1 = stringValue.toInt()
+    println("Integer value1 (From String) : $stringToInteger1")
+
+    var stringToInteger2 = stringValue.toInt()
+    println("Integer value2 (From String) : $stringToInteger2")
+
+    var stringToDouble = stringValue.toDouble()
+    println("Double Value (from String) : $stringToDouble")
+
+
+}
+
