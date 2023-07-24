6dd50b0be83d09e45011479f7ff770bd23849ce5
vikaslohar21 <vikaslohar2122@gmail.com>
2023-07-24 22:29:20 +0530

    last Remaining practical-1.8.kt

--- src/main/kotlin/practical-1/practical-1.8.kt
+++ src/main/kotlin/practical-1/practical-1.8.kt
@@ -16,12 +16,41 @@ fun main ()
 
     println("Create Array-4 by using IntArray():")
     var array4 = IntArray(5) //{i: Int -> i}
-    println(array4.contentToString())
+    println(array4.joinToString())
 
     println("Create Array-5 by using intArrayOf():")
     var array5 = intArrayOf(12,23,21,3,13)
-    println(array5.joinToString {""})
+    println(array5.joinToString())
+
+     println("Print 2-D Array-6 by using arrayOf() and intArrayOf() : ")
+     var array6 = arrayOf(intArrayOf(5,2),intArrayOf(7,6),intArrayOf(9,5))
+     println(array6.contentDeepToString())
+
+    println("Please enter Array Size : ")
+   var size : Int = readLine()!!.toInt()
+    println("Please enter Array value : ")
+    var user = IntArray(size) {0}
+
+    for (i in 0 until size)
+    {
+        print("a[$i]:")
+        user[i] = readLine()!!.toInt()
+    }
+    println("Entered array :")
+    println(user.contentToString())
+
+    println()
+    println("*********************** With Built-in Function ***********************")
+    println("After sorting by Built-in Function : ")
+    user.sort()
+    println(user.joinToString())
+
+    println()
+    println("*********************** Without Built-in Function ***********************")
+
+
 
 
 }
+
 
