fe756693a88e08fb335b906b44825e5483b28e69
vikaslohar21 <vikaslohar2122@gmail.com>
2023-07-28 13:02:27 +0530

    Completed practical-1.10.kt

--- src/main/kotlin/practical-1/practical-1.10.kt
+++ src/main/kotlin/practical-1/practical-1.10.kt
@@ -1,26 +1,51 @@
 package `practical-1`
-
-
-//class Student <String>(var name :String, var en : String)
-//{
-////    override fun toString(): kotlin.String {
-////        return super.toString()
-////    }
-//    var studentName : String = name
-//    var enrollNo : String = en
-//
-//    init {
-//        println("This is init class!")
-//    }
-//}
-
-class student (var firstName : String, var lastName: String):Person(firstName,lastName)
+class Car
+        (
+        var type : String,
+        var model : Int,
+        var Oprice : Double,
+        var Cprice : Double,
+        var owner : String,
+        var milesdrive : Int
+        )
 {
-    var name : String = firstName+ " " +lastName
+    init {
+        println("Object of class is created and Init is Called.")
+        println()
+    }
+    fun getOriginalCarPrice(): Double {
+        return Oprice
+    }
+
+    fun getCurrentCarPrice(): Double {
+        return Cprice
+    }
+
+    fun displayCarInfo() {
+        val originalPrice = getOriginalCarPrice()
+        val currentPrice = getCurrentCarPrice()
+        println("-----------------------------------")
+        println("Car Information : $type, $model")
+        println("Car Owner : $owner")
+        println("Miles Drive : $milesdrive")
+        println("Original Car Price : $originalPrice")
+        println("Current Car Price : $currentPrice")
+        println("-----------------------------------")
+    }
 }
-fun main()
-{
-//     var stu1 = Student  ("Vikas Lohar","21012011158")
-//     println(stu1.name)
-//     println(stu1.en)
+
+fun main() {
+    var car1 = Car("BMW",2018, 100000.0,98950.0, "Aman",105 )
+    var car2 = Car("BMW",2019, 400000.0,399800.0, "Karan",20 )
+    car1.displayCarInfo()
+    car2.displayCarInfo()
+
+    var cars = ArrayList<Car>()
+    cars.add(Car("Toyota",2017, 1080000.0,1079000.0, "KJS",100 ))
+    cars.add(Car("Maruti",2020, 4000000.0,3998000.0, "NPP",200 ))
+    println("********** ArrayList of Car **********")
+    for (car in cars) {
+        car.displayCarInfo()
+        println()
+    }
 }
